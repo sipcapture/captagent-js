0c2bbcc1ba56896aea039ab01cf4acedf5f2a99e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-04-04 00:05:01 +0200

    Update captagent.js

--- captagent.js
+++ captagent.js
@@ -17,20 +17,10 @@
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
 
- Requirements:
-
-	npm install cap
-	npm install sipcore
-	npm install hep-js
-	npm install elasticsearch
-
-
  Example Usage:
 
 	HEP3: 
 		nodejs captagent.js -debug true -s 127.0.0.1 -p 9063 -i 2001
-	ES:   
-		nodejs captagent.js -debug true -ES 'https://test.facetflow.io:443' -t 15
 
  Daemonize using forever:
 
@@ -39,7 +29,7 @@
 
 */ 
 
-var version = 'v0.3';
+var version = 'v0.4';
 var debug = false;
 var sipdebug = false;
 var stats = {rcvd: 0, parsed: 0, hepsent: 0, err: 0, heperr: 0 }; 
@@ -56,13 +46,6 @@ if(process.argv.indexOf("-h") != -1){
 	console.log('      -p:     	HEP3 Collector Port');
 	console.log('      -i:     	HEP3 Agent ID');
 	console.log('      -P:     	HEP3 Password');
-	console.log();
-	console.log('      -ES:    	ES _Bulk API IP    (ie: 127.0.0.1) ');
-	console.log('      -EP:    	ES _Bulk API Port  (ie: 443) ');
-	console.log('      -EI:    	ES _Bulk API Index (ie: captagent)');
-	console.log('      -ET:    	ES _Bulk API Type  (ie: captagent)');
-	console.log('      -EU:    	ES _Bulk API Auth  (ie: user:pass)');
-	console.log('      -t:     	ES _Bulk Frequency (in seconds)');
 	console.log();
 	console.log('      -debug: 	Debug Internals    (ie: -debug true)');
 	console.log('      CRTL-C: 	Exit');
@@ -81,7 +64,6 @@ if(process.argv.indexOf("-h") != -1){
 	if(process.argv.indexOf("-debug") != -1){ 
 	   debug = process.argv[process.argv.indexOf("-debug") + 1];
 	}
-
 	// HEP ARGS & DEFAULTS
 	var hep_server = 'localhost';
 	if(process.argv.indexOf("-s") != -1){ 
@@ -100,99 +82,29 @@ if(process.argv.indexOf("-h") != -1){
 	    hep_pass = process.argv[process.argv.indexOf("-P") + 1];
 	}
 
-	// ES ARGS & DEFAULTS (experimental, HTTPS default)
-	var es_on = false;
-	var es_url = 'http://127.0.0.1:9200'; 
-	var es_user = ''; 
-
-	if(process.argv.indexOf("-ES") != -1){ 
-	    es_url = process.argv[process.argv.indexOf("-ES") + 1];
-	    es_on = true;
-	}
-
-	var es_index = 'captagent'; 
-	if(process.argv.indexOf("-EI") != -1){ 
-	    es_index = process.argv[process.argv.indexOf("-EI") + 1];
-	}
-	var es_type = 'captagent'; 
-	if(process.argv.indexOf("-ET") != -1){ 
-	    es_type = process.argv[process.argv.indexOf("-ET") + 1];
-	}
-
-	if(process.argv.indexOf("-EU") != -1){ 
-	    es_user = process.argv[process.argv.indexOf("-EU") + 1];
-	}
-
-	var es_timeout = 30; 
-	if(process.argv.indexOf("-t") != -1){ 
-	    es_timeout = parseInt(process.argv[process.argv.indexOf("-t") + 1]);
-	}
-
-	var es_interval = es_timeout * 1000;
-	// var es_buffer;
-
-
 console.log('Starting JSAgent '+version);
 
 /* NODE.JS Requirements */
-
-var SIP = require('sipcore');
-var Cap = require('cap').Cap,
+var SIP = require('sipcore'),
+    Cap = require('cap').Cap,
     decoders = require('cap').decoders,
-    PROTOCOL = decoders.PROTOCOL;
-
-var HEPjs = require('hep-js');
-
-/* ELASTICSEARCH Configuration */
-if (es_on) { 
-	var elasticsearch = require('elasticsearch');
-	if (es_user.length > 1) { es_url = es_url.replace('://', '://'+es_user+'@'); }
-
-	var client = new elasticsearch.Client({
-	  hosts: [
-	      es_url
-	  ]
-	});
-}
-
+    PROTOCOL = decoders.PROTOCOL,
+    HEPjs = require('hep-js');
 
 /* HEP OUT SOCKET */ 
-
-var dgram = require('dgram');
-var socket = dgram.createSocket("udp4");
-
-
+var dgram = require('dgram'),
+    socket = dgram.createSocket("udp4");
 
 /* CAPTURE SOCKET */
-
 var c = new Cap(),
     device = Cap.findDevice(),
-    // filter = 'port 5060',
     filter = bpf_filter,
     bufSize = 10 * 1024 * 1024,
     buffer = new Buffer(65535);
 
-
-/* HTTP SOCKET */
-if (es_on){
-        var es_buffer = '';
-	setInterval(function() {
-	  if (debug) console.log("HTTP: Sending Statistics...");
-	  if (buffer.length > 1) {
-	  	// Send buffer and clear
-		sendHTTP(es_buffer+'\n');
-		es_buffer = ''; 
-	  }
-	}, es_interval);
-}
-
-
-
 /* APP START */
-
 console.log('Capturing from device '+device+ ' with BPF ('+bpf_filter+')');
 console.log('Sending HEP3 Packets to '+hep_server+':'+hep_port+' with id '+hep_id);
-if (es_on) console.log('Sending JSON Packets to '+es_url+'  _Bulk API with type '+es_type);
 
 var linkType = c.open(device, filter, bufSize, buffer);
 
@@ -206,7 +118,6 @@ c.on('packet', function(nbytes, trunc) {
   var hep_proto = { "type": "HEP", "version": 3, "payload_type": "SIP", "captureId": hep_id, "capturePass": hep_pass, "ip_family": 2};
 
   // raw packet data === buffer.slice(0, nbytes)
-
   if (linkType === 'ETHERNET') {
     var ret = decoders.Ethernet(buffer);
 
@@ -281,22 +192,8 @@ var parseSIP = function(msg, rcinfo){
 		if (sipdebug) console.log(sipmsg);
 		if (debug) console.log('CSeq: '+sipmsg.headers.cseq);
 		stats.parsed++;
-			// SEND HEP3 Packet
-			sendHEP3(sipmsg,msg, rcinfo);
-
-			if (es_on) {
-				// PARSE USERS/URI for Elasticsearch Indexing
-	                	sipmsg.headers["from_uri"] = sipmsg.headers.from.match(/^(<sip)(.*)>/)[0];
-	                	sipmsg.headers["to_uri"] = sipmsg.headers.to.match(/^(<sip)(.*)>/)[0];
-	                	sipmsg.headers["from_user"] = sipmsg.headers.from.match(/<sip:(.*?)@/)[1] ;
-	                	sipmsg.headers["to_user"] = sipmsg.headers.to.match(/<sip:(.*?)@/)[1] ;
-	                	// SESSION METHOD
-	                	sipmsg.headers["sess_method"] = sipmsg.headers.cseq.replace(/[^A-Za-z\s!?]/g,'');
-	                	// INJECT NETWORK/HEP Headers
-	                	sipmsg['hep'] = rcinfo;
-
-				bufferSIP(sipmsg);
-			}
+		// SEND HEP3 Packet
+		sendHEP3(sipmsg,msg, rcinfo);
 	} 
 	catch (e) {
 		if (debug) console.log(e);
@@ -307,7 +204,6 @@ var parseSIP = function(msg, rcinfo){
 
 
 /* HEP3 Socket OUT */
-
 var sendHEP3 = function(sipmsg,msg, rcinfo){
 	if (sipmsg) {
 		try {
@@ -324,42 +220,6 @@ var sendHEP3 = function(sipmsg,msg, rcinfo){
 		} 
 		catch (e) {
 			console.log('HEP3 Error sending!');
-			console.log(e);
-			stats.heperr++;
-		}
-	}
-}
-
-
-
-/* JSON _Bulk Buffer */
-
-var bufferSIP = function(data){
-        if (debug) console.log('Buffering SIP....');
-        var now = new Date().toISOString().substring(0, 10).replace(/-/g,'.');
-        data["@timestamp"] = new Date().toISOString().slice(0, 19) + 'Z';
-        es_buffer += '{"index":{"_index":"'+es_index+'-'+now+'","_type":"'+es_type+'"}}\n'+JSON.stringify(data)+'\n';
-}
-
-
-
-/* HTTP Socket OUT */
-
-var sendHTTP = function(xbuffer){
-	if (xbuffer && xbuffer.length > 1) {
-		try {
-                        if (debug) console.log('Sending HTTP JSON Packet...');
-                        // post the data to Bulk using ES Client
-			client.bulk({
- 				body: xbuffer 
-			}, function (err, resp) {
-				if (err) console.log('Err: ',JSON.stringify(err) );
-				else if (debug) console.log('Resp: ',JSON.stringify(resp) );
-			});
-
-		} 
-		catch (e) {
-			console.log('HTTP Error sending!');
 			console.log(e);
 			stats.heperr++;
 		}
